fc377c684e4ce95a6f6021bb2300ae2607b11bc9
Luca.Buccolini@gmail.com <Luca.Buccolini@gmail.com@327776df-391e-a89f-1b10-a49ecf5c8a44>
2015-01-09 16:09:49 +0000

    only one template for TIVA C is done and fully working!

--- FW/Eclipse_Workspace/template/Debug/main.d
+++
@@ -1,6 +0,0 @@
-main.d: ../main.c /home/luca/src/stellaris/Tivaware/inc/tm4c123gh6pm.h \
- ../app_can.h
-
-/home/luca/src/stellaris/Tivaware/inc/tm4c123gh6pm.h:
-
-../app_can.h:
--- FW/Eclipse_Workspace/template/Debug/startup_gcc.d
+++
@@ -1,7 +0,0 @@
-startup_gcc.d: ../startup_gcc.c \
- /home/luca/src/stellaris/Tivaware/inc/hw_nvic.h \
- /home/luca/src/stellaris/Tivaware/inc/hw_types.h
-
-/home/luca/src/stellaris/Tivaware/inc/hw_nvic.h:
-
-/home/luca/src/stellaris/Tivaware/inc/hw_types.h:
